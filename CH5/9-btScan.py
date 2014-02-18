ddeeb336c93dd9242afea0f57023c322537a5aeb
porterhau5 <porter.thomas.w@gmail.com>
2014-02-18 15:25:15 -0500

    Detects discoverable Bluetooth devices

---
+++ CH5/9-btScan.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import time
+from bluetooth import *
+
+alreadyFound = []
+
+
+def findDevs():
+    foundDevs = discover_devices(lookup_names=True)
+    for (addr, name) in foundDevs:
+        if addr not in alreadyFound:
+            print '[*] Found Bluetooth Device: ' + str(name)
+            print '[+] MAC address: ' + str(addr)
+            alreadyFound.append(addr)
+
+
+while True:
+    findDevs()
+    time.sleep(5)
